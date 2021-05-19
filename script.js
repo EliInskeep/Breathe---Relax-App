80b4e45386bfbdb09e4920df1bef614bfa133de1
Eli Inskeep <74996506+EliInskeep@users.noreply.github.com>
2021-05-19 04:16:48 -0500

    Music Player Updates
    Finished almost all of the CSS for the music player and started the JavaScript. Should be able to finish up the project tomorrow night.

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
+
+// Song Titles
+const songs = ['airstream - electra.mp3']
+
+
+
+
+
+
+
+
+
+
+// Breathe Section
 const container = document.querySelector('.main-container')
 const text = document.querySelector('#text')
 
